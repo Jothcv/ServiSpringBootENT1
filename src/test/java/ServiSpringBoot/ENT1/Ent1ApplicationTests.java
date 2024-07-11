d7b331d231ed9d3a30fb7738e7bde0ac9c4a4cbc
Jothcv <soulkry.0@gmail.com>
2024-07-10 21:28:47 -0600

    ENT1

---
+++ src/test/java/ServiSpringBoot/ENT1/Ent1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ServiSpringBoot.ENT1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ent1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
